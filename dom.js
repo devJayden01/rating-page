d937dc0ef58206bd30207e8c386473039576e249
Ohoke <02genasis@gmail.com>
2024-03-11 13:52:30 +0100

    first commit

---
+++ dom.js
@@ -0,0 +1,19 @@
+function validateForm(){
+
+    const number1 = document.getElementById('num1');
+    const number2 = document.getElementById('num2');
+    const number3 = document.getElementById('num3');
+    const number4 = document.getElementById('num4');
+    const number5 = document.getElementById('num5');
+
+    const firstRating = document.getElementById('select1');
+    const secondRating = document.getElementById('select2');
+    const thirdRating = document.getElementById('select3');
+    const fourthRating = document.getElementById('select4');
+    const fifthRating = document.getElementById('select5');
+
+    const firstPage = document.querySelectorAll('.containerA')
+    const nextPage = document.querySelectorAll('.containerB')
+
+
+}
